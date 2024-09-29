f98465a5504c86ece3f61ae9c0f9974fd006f5e2
PrinceStiles <princekarpture@gmail.com>
2024-09-29 20:28:11 +0000

    cors-headers

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const router = express.Router();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/files", express.static("files"));
 // const corsOptions = {
