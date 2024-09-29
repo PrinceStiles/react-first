0d7fca3ea2f4875b77ad7d8f734c05a99323d25e
PrinceStiles <princekarpture@gmail.com>
2024-09-29 19:54:14 +0000

    cors-headers

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/files", express.static("files"));
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:3000/",
   credentials: true,
 };
 
