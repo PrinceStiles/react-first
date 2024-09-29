29f7c4167ce449bdf183adeb89e74d205a0128eb
PrinceStiles <princekarpture@gmail.com>
2024-09-29 20:10:32 +0000

    cors-headers

--- backend/index.js
+++ backend/index.js
@@ -18,18 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/files", express.static("files"));
-const allowedOrigins = ["http://localhost:3000", "http://localhost:3000/"];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    // If the origin is in the allowedOrigins list or the request doesn't have an origin (like for mobile apps or Postman)
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true); // Allow the request
-    } else {
-      callback(new Error("Not allowed by CORS")); // Block the request
-    }
-  },
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  origin: ["http://localhost:3000", "http://localhost:3000/"],
 };
 
 app.use(cors(corsOptions));
